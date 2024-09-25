4117570445f8c1c1d40ab97a9383bb7288816588
CodeNewbie-pradip <pradipalabade6442@gmail.com>
2024-09-25 15:34:54 +0530

    Step -4, added course Router

--- webdev/week 8/index.js
+++ webdev/week 8/index.js
@@ -3,13 +3,12 @@ const express=require("express");
 const mongoose=require("mongoose");
 const {userRouter}=require("./routes/user");
 const {courseRouter}=require("./routes/course");
+const {adminRouter}=require("./routes/admin")
 
 const app=express();
 
 app.use("/api/v1/user", userRouter);
+app.use("/api/v1/admin", adminRouter);
 app.use("/api/v1/course", courseRouter);
-
-
-
 
 app.listen(3000);
